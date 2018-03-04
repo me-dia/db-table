084e80449c995c3f345bb6e989ce7a588d66d8ff
Your Name <gabor.tornai@shiwaforce.com>
2018-03-04 16:32:36 +0100

    readme

--- dbtest.php
+++ dbtest.php
@@ -1,37 +1,24 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-//echo (integer)ini_get('display_errors');
 
-define("DB_HOST","localhost");
-define("DB_USER","root");
-define("DB_PASSWORD","root");
-define("DB_NAME","wordpress");
+define("DB_HOST","hostname");
+define("DB_USER","username");
+define("DB_PASSWORD","password");
+define("DB_NAME","dbname");
 
 
 require_once "wordpress.php";
 
-/*$m = new mysqldb();
-$m->db_connect();
-$res = $m->query("select * from wp_posts");
-$m->db_close();
-print_r($res);
-*/
-class teszt{
 
-    public static $classname2 = "teszttttt";
-}
+$all = wordpress::find_all();
+$num = wordpress::count_all();
 
-//print_r(wordpress::find_all());exit();
-//print_r(wordpress::count_all());
-//print_r(wordpress::find_by_id(1));
-//$rec = wordpress::find_by_id(31);
-//$rec = new wordpress();
-//$rec->post_content = "juhheeeee";
-//$rec->save();
+$rec = new wordpress();
+$rec->post_content = "new content";
+$rec->save();
+
 $rec = wordpress::find_by_id(27);
-//$rec->ID = 20;
+//or $rec->ID = 20;
 
 $rec->delete();
-exit("xxxxx");
+
 ?>
